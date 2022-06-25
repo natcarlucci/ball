c32ce6fa6a04ff172e189fbf17f8101bb5f47a80
nathalia carlucci <nathalia.carlucci.7@gmail.com>
2022-06-25 13:19:55 -0300

    Circumferencia e vlome de uma esfera + PI

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Product prod = new Product();
+		
+		System.out.print("Raio: ");
+		double radius = sc.nextDouble();
+		double c = prod.circumference(radius);
+		double v = prod.volume(radius);
+		
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", prod.PI);
+
+		sc.close();
+	}
+
+}
